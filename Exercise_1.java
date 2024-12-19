f77ee2861d041debb60a2a528d91a9d764e63e8a
Geetanjali Jha <106706607+geetanjali-jha@users.noreply.github.com>
2024-12-19 09:02:59 +0530

    Update Exercise_1.java
    Completed Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -7,29 +7,49 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        return top == -1;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
+        if(top == a.length-1) {
+            System.out.println("Stack is overflow");
+            return false;
+        }
         //Write your code here
+        top++;
+        a[top] = x;
+        return  true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
+        if(isEmpty()) {
+            System.out.println("Stack is underflow");
+            return -1;
+        }
         //Write your code here
+        int poppedElement = a[top];
+        top--;
+        return poppedElement;
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(isEmpty()){
+            return  -1;
+        }
+        return a[top];
     } 
 } 
   
