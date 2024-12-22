d17b6d63350db0b5e85ef139aeb0fee048d47772
Geetanjali Jha <geetanjali.mlih@gmail.com>
2024-12-22 23:35:55 +0530

    Submitting Precourse-1.

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,46 +1,55 @@
 class Stack { 
     //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
+    //Kindly include Time and Space complexity at top of each file
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
+
+    Stack()
+    {
+        //Initialize your constructor
+        top = -1;
+    }
+
+    boolean isEmpty()
+    {
+        //Write your code here
+        return top == -1;
+    }
+
+    boolean push(int x)
+    {
         //Check for stack Overflow
+        if(top == a.length-1) {
+            System.out.println("Stack is overflow");
+            return false;
+        }
         //Write your code here
-    } 
-  
-    int pop() 
-    { 
+        top++;
+        a[top] = x;
+        return  true;
+
+    }
+
+    int pop() {
         //If empty return 0 and print " Stack Underflow"
+        if(isEmpty()) {
+            System.out.println("Stack is underflow");
+            return -1;
+        }
         //Write your code here
-    } 
-  
-    int peek() 
-    { 
+        int poppedElement = a[top];
+        top--;
+        return poppedElement;
+    }
+
+    int peek()
+    {
         //Write your code here
-    } 
-} 
-  
-// Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+        if(isEmpty()){
+            return  -1;
+        }
+        return a[top];
+    }
 }
